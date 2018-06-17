a2548ed9438c72857ee881c56910f65fd772fd72
JamesWang <jame2408@gmail.com>
2018-06-17 19:26:11 +0800

    Refactoring Test Case : Extract Method.

--- TestSumOfPositive/TestSumOfPositive.cs
+++ TestSumOfPositive/TestSumOfPositive.cs
@@ -8,17 +8,19 @@ namespace TestSumOfPositive
         [TestMethod]
         public void Input_One_Positive_Number_Array_1_Should_Be_1()
         {
-            var sumOfPositive = new SumOfPositive();
-            var sum = sumOfPositive.AddPositive(new[] { 1 });
-            Assert.AreEqual(1, sum);
+            AddShouldBe(new[] { 1 }, 1);
         }
 
         [TestMethod]
         public void Input_Two_Positive_Number_Array_1_3_Should_Be_4()
         {
+            AddShouldBe(new[] { 1, 3 }, 4);
+        }
+
+        private static void AddShouldBe(int[] inputInts, int expected)
+        {
             var sumOfPositive = new SumOfPositive();
-            var sum = sumOfPositive.AddPositive(new[] { 1, 3 });
-            Assert.AreEqual(4, sum);
+            Assert.AreEqual(expected, sumOfPositive.AddPositive(inputInts));
         }
     }
 }
